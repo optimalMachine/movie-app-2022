1fd6cd639e003602d73471e1f617b88b180ddcef
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-14 12:22:45 -0500

    updates
    updates several files

---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import axios from './axios';
+
+function Row({title}) {
+
+    const [movies,setMovies]=useState([]);
+    useEffect(()=>{
+
+    },[]);
+
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            {/* {container -> posters} */}
+        </div>
+    );
+};
+
+export default Row
